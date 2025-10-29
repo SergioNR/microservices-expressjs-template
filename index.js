50bbde3aa703c311ad7aebe333d457ed37a93a31
Sergio N <s.navarroredondo@gmail.com>
2025-10-29 23:30:28 +0100

    IMPROVE server initialisation & add graceful shutdown

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import  express from 'express'
+import { createServer } from 'http';
+import { consumeQueue } from './config/consumer.js';
+import { sendMessageToQueue } from './config/publisher.js';
 
 const app = express();
+const server = createServer(app);
 
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
+
+const gracefulShutdown = () => {
+    console.log('Received shutdown signal, closing server...');
+    server.close(() => {
+    console.log('Express server closed');
+    
+    process.exit(0);
+    });
+
+  // Force shutdown after timeout
+    setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 10000);
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
