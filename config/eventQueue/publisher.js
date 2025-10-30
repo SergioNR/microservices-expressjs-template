2ad1b74934d45560c9814753518045e9a27e960f
Sergio N <s.navarroredondo@gmail.com>
2025-10-30 12:46:04 +0100

    TODO - wip publisher

--- config/eventQueue/publisher.js
+++ config/eventQueue/publisher.js
@@ -1,13 +1,10 @@
-import { connectToEventQueue } from "./queueConnection.js";
+import { connectToAMQP } from "./queueConnection.js";
 
-export const sendMessageToQueue = async (queueName, message) => {
+export const sendMessageToExchange = async (queueName, message) => {
     try {
-        const channel = await connectToEventQueue();
-        const queue = await channel.queue(queueName);
         
-        await queue.publish(message, { 
-            deliveryMode: 2 
-        });
+        const messageSent = await channel.basicPublish('exampleExchange', 'exampleQueue', 'exampleData')
+        
 
     } catch (error) {
         console.error('Error sending message to queue:', error);
